4cbb5ebb8439d1b2428bc5e44fdb80c7be76bb10
Scheufler, Henning <henning.scheufler@dlr.de>
2022-07-18 15:48:00 +0200

    ENH: runner can call mulitple cases in parallel

--- src/casefoam/cli/run.py
+++ src/casefoam/cli/run.py
@@ -3,16 +3,46 @@ from pathlib import Path
 from casefoam.utility import of_cases
 import os
 import subprocess
-import time
-from rich.progress import track
+from rich.progress import Progress
+from concurrent.futures import ThreadPoolExecutor
+import asyncio
+from random import random
+from time import sleep
 
 app = typer.Typer()
+
+
+def execute_script(script_name, case_path, progress, task_bar):
+    sleep_t = random() + 0.1
+    sleep(sleep_t)
+    os.chdir(case_path)
+    subprocess.run(f"./{script_name}", stdout=subprocess.DEVNULL)
+    progress.update(task_bar, advance=1)
+
+
+async def run_case(cases, script_name, cwd, nProcs):
+    with Progress() as progress:
+        prog_bar = progress.add_task("running cases...", total=len(cases))
+        with ThreadPoolExecutor(max_workers=nProcs) as pool:
+            loop = asyncio.get_running_loop()
+            futures = [
+                loop.run_in_executor(
+                    pool, execute_script, script_name, (cwd / case), progress, prog_bar
+                )
+                for case in cases
+            ]
+            try:
+                await asyncio.gather(*futures, return_exceptions=False)
+            except Exception as ex:
+                print("Caught error executing task", ex)
+                raise
 
 
 @app.command()
 def folder(
     folder: str,
     script_name=typer.Option("Allrun", help="specify to run in all of cases"),
+    nProcs: int = typer.Option(1, "--nProcs"),
 ):
     p = Path.cwd() / folder
     cwd = Path.cwd()
@@ -21,8 +51,12 @@ def folder(
 
     cases = of_cases(folder)
     total = 0
-    for case in track(cases, description="running cases..."):
-        os.chdir(cwd / case)
-        subprocess.run(f"./{script_name}")
-        total += 1
-    print(f"run {total} OpenFOAM Cases.")
+    #     for case in track(cases, description="running cases..."):
+    #         os.chdir(cwd / case)
+    #         subprocess.run(f"./{script_name}")
+    #         total += 1
+    loop = asyncio.get_event_loop()
+    coroutine = run_case(cases, script_name, cwd, nProcs)
+    loop.run_until_complete(coroutine)
+
+    print(f"run {len(cases)} OpenFOAM Cases.")
