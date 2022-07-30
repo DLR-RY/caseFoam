605ad1c24044b6361a840496bd8f834809aec778
Scheufler, Henning <henning.scheufler@dlr.de>
2022-07-30 16:16:21 +0200

    ENH: casefoam auto creates getData.py

--- src/casefoam/cli/post.py
+++ src/casefoam/cli/post.py
@@ -5,14 +5,40 @@ import os
 import subprocess
 import rich
 import json
-# from rich.pretty import pprint
-# from rich.progress import Progress
-# from concurrent.futures import ThreadPoolExecutor
-# import asyncio
-# from random import random
-# from time import sleep
 
 app = typer.Typer()
+
+def create_functionobjects(fo_name,file,case_path):
+    file_stem = Path(file).stem
+    f_objects = f"""
+{fo_name}_{file_stem} = casefoam.load_functionObject("{fo_name}","{file}",OfCases="{case_path}")
+{fo_name}_{file_stem}.to_csv("results/{fo_name}_{file_stem}.csv",index=False)
+
+"""
+    return f_objects
+
+
+def create_get_data(case_path):
+    getData = []
+    header = f"""
+import casefoam
+from pathlib import Path
+
+#auto generated post processing of the data acquisition
+
+#create results dir to save all data
+Path("results").mkdir(exist_ok=True)
+p_ofcases = "{case_path}"
+
+"""
+    getData.append(header)
+    function_objects = list_functionObjects(case_path)
+
+    for fo_name, files in function_objects.items():
+        for file in files:
+            getData.append(create_functionobjects(fo_name,file,case_path))
+
+    return "".join(getData)
 
 
 @app.command()
@@ -26,3 +52,10 @@ def function_objects(folder: str):
 
     rich.print_json(json.dumps(function_objects,indent=2))
 
+@app.command()
+def template_getData(case_folder: str):
+
+    getData = create_get_data(case_folder)
+
+    with open("getData.py","w") as f:
+        f.write(getData)
